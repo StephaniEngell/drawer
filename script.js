72f9dc04269b000c4b154e80b1fbea079b2b5b2b
Stephani Engel <stephaniengel18@gmaile.com>
2023-07-08 22:42:11 -0300

    adding rules to draw the numbers

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ function genereteNumber(){
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if(min >= max) {
+        alert('O valor mínimo tem que ser MENOR que o valor máximo')
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(result)
+        alert(result)    
+    }
+
 
 }
